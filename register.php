1fa01ccdd3c17e9be485b975129e0c9faf0522a5
irvan naufal <irvannaufal11@gmail.com>
2018-10-27 19:41:15 +0700

    Tambah Register

---
+++ register.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+	<link rel="stylesheet" type="text/css" href="lib/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/style-login-reg.css">
+	<script type="text/javascript" src="lib/js/bootstrap.min.js"></script>
+</head>
+<body id="LoginForm">
+	<div id="container">
+	<section class="login-inner">
+	    <div class="login-middle">
+	      <div class="login-details">
+	      	<img src="lib/img/face.jpg" class="profile-img-card">
+	        <form>
+	          <label>
+	            <input type="text" placeholder="Username" name="uname" required="">
+	          </label>
+	          <label>
+	            <input type="text" placeholder="Nama" name="name">
+	            * Nama akan ditampilkan publik
+	          </label>
+	          <label>
+	            <input type="text" placeholder="Email" name="email">
+	          </label>
+	          <label>
+	            <input type="text" placeholder="Nomor Telepon" name="nm-telp">
+	            * Nomor telepon akan ditampilkan publik pada beberapa kategori
+	          </label>
+	          
+	          <label>
+	            <input type="password" placeholder="Password" name="passwd">
+	          </label>
+	          <label class="checkbox-inline">
+	          	<input type="checkbox" value=""> Saya setuju dengan <a href="#"> Syarat dan Ketentuan</a> yang telah ditentukan</label>
+	          <div class="btn-sub" align="center">
+	            <button class="btn btn-primary">Daftar</button>
+	            <br>
+	            <div class="forgot">
+	              <a href="#">Lupa Kata Sandi ?</a>
+	            </div>
+	          </div>
+
+	        </form>
+	      </div>
+	    </div>
+  </section>
+
+</div>
+</div>
+</body>
+</html>
