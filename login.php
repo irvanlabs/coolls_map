b0c57e2025f5340523a32a7d10cf007605695586
irvan naufal <irvannaufal11@gmail.com>
2018-10-27 19:12:06 +0700

    Update Login

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="lib/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/style.css">
+	<script type="text/javascript" src="lib/js/bootstrap.min.js"></script>
+</head>
+<body id="LoginForm">
+	<div id="container">
+	<section class="login-inner">
+	    <div class="login-middle">
+	      <div class="login-details">
+	      	<img src="lib/img/face.jpg" class="profile-img-card">
+	        <form>
+	          <label>
+	            <input type="text" placeholder="Alamat Email" name="email">
+	          </label>
+	          <label>
+	            <input type="text" placeholder="Nomor Telepon" name="nm-telp">
+	          </label>
+	          <label>
+	            <input type="password" placeholder="Password" name="passwd">
+	          </label>
+	          <div class="btn-sub" align="center">
+	            <button class="btn btn-primary">Log In</button>
+	            <br>
+	            <div class="forgot">
+	              <a href="#">Lupa Kata Sandi ?</a>
+	            </div>
+	          </div>
+
+	        </form>
+	      </div>
+	    </div>
+  </section>
+
+</div>
+</div>
+</body>
+</html>
