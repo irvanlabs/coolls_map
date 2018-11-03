9acb07328d6c2d51a20a71b252fa6d7330d8c278
irvan naufal <irvannaufal11@gmail.com>
2018-11-03 08:25:29 +0700

    tambah hanya untuk layout

--- login.php
+++ login.php
@@ -1,41 +1,52 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="lib/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="lib/css/style-login-reg.css">
-	<script type="text/javascript" src="lib/js/bootstrap.min.js"></script>
-</head>
-<body id="LoginForm">
-	<div id="container">
-	<section class="login-inner">
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="initial-scale=1, shrink-to-fit=no, width=device-width" name="viewport">
+
+    <!-- CSS -->
+    <!-- Add Material font (Roboto) and Material icon as needed -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Add Material CSS, replace Bootstrap CSS -->
+    <link href="css/material.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style-login-reg.css">
+  </head>
+  <body>
+  	<section class="login-inner">
 	    <div class="login-middle">
 	      <div class="login-details">
 	      	<img src="lib/img/face.jpg" class="profile-img-card">
-	        <form>
-	          <label>
-	            <input type="text" placeholder="Alamat Email" name="email">
-	          </label>
-	          <label>
-	            <input type="text" placeholder="Nomor Telepon" name="nm-telp">
-	          </label>
-	          <label>
-	            <input type="password" placeholder="Password" name="passwd">
-	          </label>
-	          <div class="btn-sub" align="center">
-	            <button class="btn btn-primary">Log In</button>
-	            <br>
-	            <div class="forgot">
-	              <a href="#">Lupa Kata Sandi ?</a>
-	            </div>
-	          </div>
+   		 		<form>
+			  <div class="form-group">
+				    <label>Email address</label>
+				    <input type="email" class="form-control" id="" aria-describedby="emailHelp" placeholder="Masukkan email">
+				    <small id="emailHelp" class="form-text text-muted">Kami tidak akan memberotahukan email anda pada siapapun.</small>
+				  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Password</label>
+			    <input type="password" class="form-control"placeholder="Password">
+			  </div>
+			  <div class="form-group">
+				    <div class="forgot">
+				    	<a href="#">Lupa kata sandi ?</a>
+				    </div>
+			  </div>
+			  <button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+			</div>
+		</div>
+	</section>
 
-	        </form>
-	      </div>
-	    </div>
-  </section>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
-</div>
-</div>
-</body>
+    <!-- Then Material JavaScript on top of Bootstrap JavaScript -->
+    <script src="js/material.min.js"></script>
+  </body>
 </html>
