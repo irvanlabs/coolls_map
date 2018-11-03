4aed25b9c4b53944ea748d4f0d0b7a21a002ff08
irvan naufal <irvannaufal11@gmail.com>
2018-11-03 08:16:15 +0700

    ubah struktur folder

---
+++ old/contributors/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wealah</title>
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../lib/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+</body>
+</html>
